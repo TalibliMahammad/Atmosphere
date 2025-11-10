bb27a46dcce164e3fe862ebe363fc1f9fcb91512
TalibliMahammad <mahammadtalibli@mail.ru>
2025-11-10 23:44:44 +0400

    solved header problem

--- src/components/city-search.tsx
+++ src/components/city-search.tsx
@@ -1,4 +1,4 @@
-import  { useState } from 'react'
+import { useState } from 'react'
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from './ui/command'
 import { Button } from './ui/button'
 import { Clock, Loader2, Search, Star, XCircle } from 'lucide-react'
@@ -58,7 +58,8 @@ const CitySearch = () => {
                 <CommandInput
                     value={query}
                     onValueChange={setQuery}
-                    placeholder="Search cities...." />
+                    placeholder="Search cities...."
+                     />
 
                 <CommandList>
                     {query.length > 2 && !isLoading && <CommandEmpty>No results found.</CommandEmpty>}
@@ -99,80 +100,80 @@ const CitySearch = () => {
                         </>
 
                     )}
-            
-
-                {history.length > 0 && (
-                    <>
-
-                        <CommandSeparator />
-                        <CommandGroup>
-                            <div className='flex items-center justify-between px-2 my-2'>
-                                <p className='text-xs text-muted-foreground'>
-                                    Recent searches
-                                </p>
-                                <Button variant="ghost" size="sm" onClick={() => { clearHistory.mutate() }}>
-                                    <XCircle className="mr-2 h-4 w-4" />
-                                    Clear
-                                </Button>
-                            </div>
-                            {history.map((city) => {
-                                return (
-                                    <CommandItem
-
-                                        value={`${city.lat}|${city.lon}|${city.name}|${city.country ?? 'Unknown'}`}
-
-                                        onSelect={handleSelect}
-                                        key={`${city.lat}-${city.lon}}`}>
-
-                                        <Clock className="mr-2 h-4 w-4" />
-                                        <span>{city.name}</span>
-                                        {city.state && (
-                                            <span className='text-sm text-muted-foreground'>,{city.state}</span>
-                                        )}
-                                        <span className='text-sm text-muted-foreground'>, {city.country}</span>
-                                        <span className='ml-auto text-xs text-muted-foreground'>
-                                            {format(city.searchedAt, "MMM dd, yyyy")}
-                                        </span>
-                                    </CommandItem>
-                                )
-                            })}
-                        </CommandGroup>
-                        <CommandSeparator />
-                    </>
-
-                )}
 
 
-                {locations && locations.length > 0 && (<CommandGroup heading="Suggestions">
-                    {isLoading && (
-                        <div className='flex items-center justify-center p-4'>
-                            <Loader2 className="animate-spin h-4 w-4" />
-                        </div>
+                    {history.length > 0 && (
+                        <>
+
+                            <CommandSeparator />
+                            <CommandGroup>
+                                <div className='flex items-center justify-between px-2 my-2'>
+                                    <p className='text-xs text-muted-foreground'>
+                                        Recent searches
+                                    </p>
+                                    <Button variant="ghost" size="sm" onClick={() => { clearHistory.mutate() }}>
+                                        <XCircle className="mr-2 h-4 w-4" />
+                                        Clear
+                                    </Button>
+                                </div>
+                                {history.map((city) => {
+                                    return (
+                                        <CommandItem
+
+                                            value={`${city.lat}|${city.lon}|${city.name}|${city.country ?? 'Unknown'}`}
+
+                                            onSelect={handleSelect}
+                                            key={`${city.lat}-${city.lon}}`}>
+
+                                            <Clock className="mr-2 h-4 w-4" />
+                                            <span>{city.name}</span>
+                                            {city.state && (
+                                                <span className='text-sm text-muted-foreground'>,{city.state}</span>
+                                            )}
+                                            <span className='text-sm text-muted-foreground'>, {city.country}</span>
+                                            <span className='ml-auto text-xs text-muted-foreground'>
+                                                {format(city.searchedAt, "MMM dd, yyyy")}
+                                            </span>
+                                        </CommandItem>
+                                    )
+                                })}
+                            </CommandGroup>
+                            <CommandSeparator />
+                        </>
+
                     )}
 
-                    {locations.map((location) => {
 
-                        return (
-                            <CommandItem
-                                value={`${location.lat}|${location.lon}|${location.name}|${location.country ?? 'Unknown'}`}
+                    {locations && locations.length > 0 && (<CommandGroup heading="Suggestions">
+                        {isLoading && (
+                            <div className='flex items-center justify-center p-4'>
+                                <Loader2 className="animate-spin h-4 w-4" />
+                            </div>
+                        )}
 
-                                onSelect={handleSelect}
-                                key={`${location.lat}-${location.lon}}`}>
-                                <Search className="mr-2 h-4 w-4" />
-                                <span>{location.name}</span>
-                                {location.state && (
-                                    <span className='text-sm text-muted-foreground'>,{location.state}</span>
-                                )}
-                                <span className='text-sm text-muted-foreground'>, {location.country}</span>
-                            </CommandItem>
-                        )
+                        {locations.map((location) => {
 
-                    })}
+                            return (
+                                <CommandItem
+                                    value={`${location.lat}|${location.lon}|${location.name}|${location.country ?? 'Unknown'}`}
+
+                                    onSelect={handleSelect}
+                                    key={`${location.lat}-${location.lon}}`}>
+                                    <Search className="mr-2 h-4 w-4" />
+                                    <span>{location.name}</span>
+                                    {location.state && (
+                                        <span className='text-sm text-muted-foreground'>,{location.state}</span>
+                                    )}
+                                    <span className='text-sm text-muted-foreground'>, {location.country}</span>
+                                </CommandItem>
+                            )
+
+                        })}
 
 
-                </CommandGroup>)}
-            </CommandList>
-        </CommandDialog >
+                    </CommandGroup>)}
+                </CommandList>
+            </CommandDialog >
         </>
     )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 
+import React from 'react'
 import { CiLight } from "react-icons/ci";
 import { FaMoon } from "react-icons/fa";
 import { TiWeatherWindy } from "react-icons/ti";
@@ -15,15 +16,16 @@ const Header = () => {
 
     return (
         <header className='sticky top-0 z-50 w-full bg-background/95 background-vlur py-2 supports-[backdrop-filter]:bg-background/60 '>
-            <div className='container mx-auto flex h-16  items-center justify-between px-4'>
-                <Link to="/" className='flex items-center md:gap-5 gap-1  '>
-                    <TiWeatherWindy className={` md:h-10 h-8 md:w-10 w-7 ${isDark ? "text-white" : "text-black"}`} />
-                    <span className={` md:text-[30px] text-[15px] font-bold flex items-center h-10 w-10 ${isDark ? "text-muted-foreground" : "text-black"}`}>Atmosphere</span>
+            <div className='container mx-auto flex h-16  gap-5 items-center  md:justify-between justify-center md:px-4'>
+                <Link to="/" className='flex items-center md:gap-5  '>
+                    <TiWeatherWindy className={`md:h-10 h-7 md:w-10 w-7 ${isDark ? "text-white" : "text-black"}`} />
+                    <span className={`flex items-center md:text-2xl text-[12px] font-bold  ${isDark ? "text-muted-foreground" : "text-black"}`}>Atmosphere</span>
                 </Link>
 
                 <div className='flex gap-4'>
-                    <CitySearch />
-
+                    <div>
+                        <CitySearch />
+                    </div>
                     <div onClick={() => setTheme(isDark ? "light" : "dark")} className='cursor-pointer'>
                         {isDark ? <CiLight className='size-8 transition duration-500 ease-in-out hover:rotate-180 ' /> : <FaMoon className='size-8 transition duration-500 ease-in-out hover:rotate-190 ' />}
                     </div>
